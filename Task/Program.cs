4178779b780672fadf598388572811de165cdc23
Elena Frolova <missis-fev@yandex.ru>
2022-12-07 12:26:13 +0300

    добавлена папка Task

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿//Программа, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше,
+// либо равна 3 символам. 
+// Первоначальный массив задан на старте выполнения алгоритма.
+
+string[] array = new string[] { "Hello", "2", "world", ":-" }; 
+string[] result = ArrayLess(array, 3);
+
+string[] ArrayLess(string[] input, int n) {
+    string[] output = new string[Count(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int Count(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
